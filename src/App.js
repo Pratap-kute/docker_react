099aa0f7d052c44f6d051d35ed75db8a4aef3288
pratap <pratap.kute97@gmail.com>
2021-06-04 15:35:59 +0530

    changed the index message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How r you
+          Hi how R You 
         </p>
         <a
           className="App-link"
